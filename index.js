6b5c379634b3139b326644ebcfb05f2993af9977
Andy Kho <48148008+wokesdev@users.noreply.github.com>
2023-07-08 04:41:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var http = require('http');
+import { getToken, getPost } from '@threadjs/threads.js';
 http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
+    await getToken('kuramanimecom', 'wedontknow123!').then(async token => {
+    	await getUser('25025320', token).then(user => {
+    		console.log(user)
+    	})
+    })
+    // console.log(`Just got a request at ${req.url}!`)
+    res.write(user);
     res.end();
 }).listen(process.env.PORT || 3000);
